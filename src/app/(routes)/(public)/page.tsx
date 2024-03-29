9c875a326eaf7fd11fb043a5c2ca75e6ef594031
Carlos Adriano <cadrianogamac@gmail.com>
2024-03-29 17:49:13 -0300

    feat(app): create login page

--- src/app/(routes)/(public)/page.tsx
+++ src/app/(routes)/(public)/page.tsx
@@ -1,9 +1,58 @@
+'use client';
+
+import { BookBookmark, Envelope, Key } from '@phosphor-icons/react';
+
 const Page = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <a href='/account' className='btn'>
-        Login
-      </a>
+    <main className='relative flex min-h-screen items-center justify-center bg-[url("/landscape.jpg")] bg-cover bg-center'>
+      <div className='z-10 flex w-[min(26rem,100%)] flex-col gap-4 rounded-3xl px-4 py-12 sm:gap-8 sm:px-8 sm:backdrop-blur'>
+        <div className='flex items-center justify-center gap-2 p-2 text-[min(3.75rem,15vw)] text-neutral'>
+          <BookBookmark className='shrink-0' />
+          <h1>Chronicler</h1>
+        </div>
+        <form className='flex w-full flex-col gap-4'>
+          <label
+            htmlFor='email'
+            aria-label='Email'
+            className='input input-bordered flex items-center gap-2'
+          >
+            <Envelope weight='fill' className='shrink-0' />
+            <input
+              id='email'
+              type='text'
+              className='min-w-0 grow'
+              placeholder='Email'
+            />
+          </label>
+          <label
+            htmlFor='password'
+            aria-label='Senha'
+            className='input input-bordered flex items-center gap-2'
+          >
+            <Key weight='fill' className='shrink-0' />
+            <input
+              id='password'
+              type='text'
+              className='min-w-0 grow'
+              placeholder='Senha'
+            />
+          </label>
+          <button type='submit' className='btn btn-neutral w-full'>
+            Login
+          </button>
+        </form>
+      </div>
+      <div className='absolute inset-0 backdrop-blur sm:hidden' />
+      <footer className='absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent px-4 py-2 text-center text-xl text-white'>
+        Fotografia de{' '}
+        <a href='https://unsplash.com/pt-br/@leo_visions' className='link'>
+          Leo_Visions
+        </a>{' '}
+        na{' '}
+        <a href='https://unsplash.com/pt-br' className='link'>
+          Unsplash
+        </a>
+      </footer>
     </main>
   );
 };
