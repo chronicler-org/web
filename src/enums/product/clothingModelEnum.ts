e5914417c9bb773056fbf210f6bca656c4793dd8
Ádrian Gama <adrian.gama.se@gmail.com>
2024-04-05 13:29:10 -0300

    chore(product): add clothing model enumerator

---
+++ src/enums/product/clothingModelEnum.ts
@@ -0,0 +1,12 @@
+export enum ClothingModel {
+  TShirt = 'Camiseta',
+  Shirt = 'Camisa',
+  Pants = 'Calça',
+  Skirt = 'Saia',
+  Dress = 'Vestido',
+  Jacket = 'Casaco',
+  Sportswear = 'Roupas Esportivas',
+  Underwear = 'Roupa Íntima',
+  Swimwear = 'Roupa de Banho',
+  FormalWear = 'Roupa Formal',
+}
