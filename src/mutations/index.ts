c4b34adb66cc1cb5051fd601a16d5903ad1b3265
inacio <inacio@pop-os.localdomain>
2024-04-07 19:24:49 -0300

    feat: Ajuste nas importações

--- src/mutations/index.ts
+++ src/mutations/index.ts
@@ -2,5 +2,7 @@ export * from './attendant';
 export * from './customer';
 export * from './manager';
 export * from './product';
+export * from './sale';
 export * from './tag';
 export * from './team';
+export * from './customerCare';
